1c3eaa4f8fb9047ba6f8b46816168e518744dd0a
Bridget <bridgetmeyboom@gmail.com>
2021-05-10 20:39:02 -0700

    Third initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET 	-1 // This display does not have a reset pin accessible
