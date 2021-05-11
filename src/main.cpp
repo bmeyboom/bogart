0c0adaad615311c18133e291d55cc8de267e919c
Bridget <bridgetmeyboom@gmail.com>
2021-05-10 20:28:46 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET 	-1 // This display does not have a reset pin accessible
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+ 
+  // Displays Adafruit logo by default. call clearDisplay immediately if you don't want this.
+  display.display();
+  delay(2000);
+
+  // Displays "Hello world!" on the screen
+  display.clearDisplay();
+  display.setTextSize(0.3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("Calum...");
+  display.println("is a little bitch!");
+  display.display();
+}
+
+void loop() {};
